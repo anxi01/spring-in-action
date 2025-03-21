4502610a9901da9571f1b33d1fdfe54ce9ff22d7
anxi01 <tjdals2893@naver.com>
2025-03-21 16:01:29 +0900

    FEAT: (#3) Order 엔티티를 생성한다

---
+++ src/main/java/spring/springinaction/tacos/domain/model/Order.java
@@ -0,0 +1,53 @@
+package spring.springinaction.tacos.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date placedAt;
+
+    private String deliveryName;
+
+    private String deliveryStreet;
+
+    private String deliveryCity;
+
+    private String deliveryState;
+
+    private String deliveryZip;
+
+    private String ccNumber;
+
+    private String ccExpiration;
+
+    private String ccCVV;
+
+    @ManyToMany(targetEntity = Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
+    }
+}
