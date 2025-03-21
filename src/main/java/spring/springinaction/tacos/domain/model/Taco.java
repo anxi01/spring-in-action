7e41a7880e0c4a9df635b817727e49a5728fd5ab
anxi01 <tjdals2893@naver.com>
2025-03-21 16:01:12 +0900

    FEAT: (#3) Taco 엔티티를 생성한다

---
+++ src/main/java/spring/springinaction/tacos/domain/model/Taco.java
@@ -0,0 +1,33 @@
+package spring.springinaction.tacos.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+public class Taco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date createdAt;
+
+    @NotNull
+    @Size(min = 5, message = "이름은 최소 5글자 이상이어야 합니다.")
+    private String name;
+
+    @ManyToMany(targetEntity = Ingredient.class)
+    @Size(min = 1, message = "최소 1개 이상의 재료가 선택해야 합니다.")
+    private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
+}
