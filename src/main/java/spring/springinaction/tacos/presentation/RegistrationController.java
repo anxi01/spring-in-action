ba694998208d6d1eeb55be77650b278f19becd6c
anxi01 <tjdals2893@naver.com>
2025-03-23 21:14:44 +0900

    FEAT: (4.4) JWT 토큰 생성, 검증 및 가입시 JWT 토큰을 반환한다

--- src/main/java/spring/springinaction/tacos/presentation/RegistrationController.java
+++ src/main/java/spring/springinaction/tacos/presentation/RegistrationController.java
@@ -3,9 +3,17 @@ package spring.springinaction.tacos.presentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.springinaction.tacos.domain.model.User;
 import spring.springinaction.tacos.domain.repository.UserRepository;
 import spring.springinaction.tacos.request.RegistrationRequest;
+import spring.springinaction.utils.JwtUtils;
+import spring.springinaction.utils.JwtUtils.Payload;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/register")
@@ -14,10 +22,17 @@ public class RegistrationController {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
 
     @PostMapping
-    public ResponseEntity<Void> processRegistration(@RequestBody RegistrationRequest request) {
-        userRepository.save(request.toUser(passwordEncoder));
-        return ResponseEntity.ok().build();
+    public ResponseEntity<String> processRegistration(@RequestBody RegistrationRequest request) {
+        User user = userRepository.save(request.toUser(passwordEncoder));
+
+        Set<Payload> payloads = Set.of(
+                new Payload("userId", user.getId().toString()),
+                new Payload("username", user.getUsername())
+        );
+        String accessToken = jwtUtils.createAccessToken(payloads);
+        return ResponseEntity.ok(accessToken);
     }
 }
