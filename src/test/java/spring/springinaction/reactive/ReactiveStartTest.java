cc3ca25df19e0da2e4a87f0a4cb6933303b84ee4
anxi01 <tjdals2893@naver.com>
2025-03-24 05:17:05 +0900

    TEST: (10.3.1) 리액티브 타입 조합하기

--- src/test/java/spring/springinaction/reactive/ReactiveStartTest.java
+++ src/test/java/spring/springinaction/reactive/ReactiveStartTest.java
@@ -3,10 +3,14 @@ package spring.springinaction.reactive;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 class ReactiveStartTest {
@@ -99,4 +103,79 @@ class ReactiveStartTest {
                 .expectNext(0L, 1L, 2L, 3L, 4L, 5L)
                 .verifyComplete();
     }
+
+    /**
+     * foodFlux가 characterFlux 다음에 스트리밍을 진행하도록
+     * delaySubscription()을 사용해 250ms 이후에 구독 및 데이터를 방출하게 함.
+     */
+    @Test
+    void Flux_결합하기() {
+        Flux<String> characterFlux = Flux
+                .just("짱구", "철수", "맹구")
+                .delayElements(Duration.ofMillis(500));
+        Flux<String> foodFlux = Flux
+                .just("제육", "돈까스", "커피")
+                .delaySubscription(Duration.ofMillis(250))
+                .delayElements(Duration.ofMillis(500));
+
+        Flux<String> mergedFlux = characterFlux.mergeWith(foodFlux);
+
+        StepVerifier.create(mergedFlux)
+                .expectNext("짱구", "제육", "철수", "돈까스", "맹구", "커피")
+                .verifyComplete();
+    }
+
+    @Test
+    void ZIP으로_결합하기() {
+        Flux<String> characterFlux = Flux
+                .just("짱구", "철수", "맹구");
+        Flux<String> foodFlux = Flux
+                .just("제육", "돈까스", "커피");
+
+        Flux<Tuple2<String, String>> mergedFlux = Flux.zip(characterFlux, foodFlux);
+
+        StepVerifier.create(mergedFlux)
+                .expectNextMatches(p ->
+                    p.getT1().equals("짱구") && p.getT2().equals("제육")
+                )
+                .expectNextMatches(p ->
+                        p.getT1().equals("철수") && p.getT2().equals("돈까스")
+                )
+                .expectNextMatches(p ->
+                        p.getT1().equals("맹구") && p.getT2().equals("커피")
+                )
+                .verifyComplete();
+    }
+
+    @Test
+    void ZIP으로_커스텀하게_결합하기() {
+        Flux<String> characterFlux = Flux
+                .just("짱구", "철수", "맹구");
+        Flux<String> foodFlux = Flux
+                .just("제육", "돈까스", "커피");
+
+        Flux<String> mergedFlux = Flux.zip(characterFlux, foodFlux, (c, f) -> c + " " + f);
+
+        StepVerifier.create(mergedFlux)
+                .expectNext("짱구 제육")
+                .expectNext("철수 돈까스")
+                .expectNext("맹구 커피")
+                .verifyComplete();
+    }
+
+    @Test
+    void 먼저_값을_방출하는_값을_발행하기() {
+        Flux<String> characterFlux = Flux
+                .just("짱구", "철수", "맹구")
+                .delaySubscription(Duration.ofMillis(100));
+        Flux<String> foodFlux = Flux
+                .just("제육", "돈까스", "커피");
+
+        Flux<String> mergedFlux = Flux.firstWithSignal(characterFlux, foodFlux);
+
+        StepVerifier.create(mergedFlux)
+                .expectNext("제육", "돈까스", "커피")
+                .verifyComplete();
+    }
+
 }
